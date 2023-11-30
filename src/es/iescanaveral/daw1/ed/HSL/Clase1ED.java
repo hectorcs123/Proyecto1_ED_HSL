5f397c7993866333d3d3e9658f08e2537c5dc305
Hector <hectorchatgpt@hotmail.com>
2023-11-30 11:38:17 +0100

    Primer comit con Eclipse

---
+++ src/es/iescanaveral/daw1/ed/HSL/Clase1ED.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package es.iescanaveral.daw1.ed.HSL;
+
+/*
+ * @author DAW1: Hector Sánchez Lizcano - IES El Cañaveral
+ * Fecha: 1 nov 2023
+*/
+public class Clase1ED {
+	public static void main(String[]args) {
+		Clase2ED clase2 = new Clase2ED();
+		clase2.saludar("HSL");
+
+	}
+}
---
+++ src/es/iescanaveral/daw1/ed/HSL/Clase2ED.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package es.iescanaveral.daw1.ed.HSL;
+
+/*
+ * @author DAW1: Hector Sánchez Lizcano - IES El Cañaveral
+ * Fecha: 1 nov 2023
+*/
+public class Clase2ED {
+	public void saludar(String HSL){
+		
+		String textoSaludo= "Hola, soy " + HSL + " imprimiendo un mensaje editado con Eclipse y creando un repositorio.";
+		System.out.println(textoSaludo);
+	
+	}
+}
