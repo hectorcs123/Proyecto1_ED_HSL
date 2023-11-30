6faa3fa85e42d326137a4b52768e199df371af81
hectorcs123 <147397201+hectorcs123@users.noreply.github.com>
2023-11-30 12:15:47 +0100

    COMMIT DESPUES DE AÑADIR COMENTARIO

--- src/es/iescanaveral/daw1/ed/HSL/Clase1ED.java
+++ src/es/iescanaveral/daw1/ed/HSL/Clase1ED.java
@@ -7,6 +7,7 @@ package es.iescanaveral.daw1.ed.HSL;
  * @author DAW1: Hector Sánchez Lizcano - IES El Cañaveral
  * Fecha: 1 nov 2023
 */
+//VAMOS A AÑADIR COMENTARIO EN LA CLASE CON EL METODO MAIN PARA REALIZAR UN PUSH
 public class Clase1ED {
 	public static void main(String[]args) {
 		Clase2ED clase2 = new Clase2ED();
